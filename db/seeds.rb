77fcc76f63d030e4f06ca4ebb0053b5d740d7262
github username <github email address>
2019-11-05 17:06:45 -0800

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@artist = Artist.create(name: "My Artist", bio: "My artist bio")
+@genre = Genre.create(name: "My Genre")
+Song.create(name: "My Song", artist_id: @artist.id, genre_id: @genre.id)
